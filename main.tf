ae2194ae29b1519b7727c1eea6a3111411ea6615
saranvas <saran666an@gmail.com>
2025-07-23 14:14:51 +0530

    Fix s3 public access block

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
 # A unique name for your S3 bucket.
-# IMPORTANT: S3 bucket names must be globally unique. Change "your-unique-name-12345" to something unique!
 variable "bucket_name" {
   description = "The name for the S3 bucket"
   type        = string
-  default     = "saranvas-portfolio-site-23072025" 
+  default     = "saranvas-portfolio-site-23072025" # Make sure this is your unique name
 }
 
 # Configure the AWS provider
 provider "aws" {
-  region = "us-east-1" # You can choose a different region if you like
+  region = "us-east-1"
 }
 
 # Resource 1: Create the S3 bucket
@@ -16,7 +15,18 @@ resource "aws_s3_bucket" "portfolio_bucket" {
   bucket = var.bucket_name
 }
 
-# Resource 2: Configure the S3 bucket to host a static website
+# (NEW!) Resource 2: Configure the public access block
+# This tells AWS to allow public access for this bucket.
+resource "aws_s3_bucket_public_access_block" "portfolio_bucket_public_access" {
+  bucket = aws_s3_bucket.portfolio_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Resource 3: Configure the S3 bucket to host a static website
 resource "aws_s3_bucket_website_configuration" "portfolio_website" {
   bucket = aws_s3_bucket.portfolio_bucket.id
 
@@ -25,9 +35,12 @@ resource "aws_s3_bucket_website_configuration" "portfolio_website" {
   }
 }
 
-# Resource 3: Create a policy to make the bucket contents public (so people can see your site)
+# Resource 4: Create a policy to make the bucket contents public
 resource "aws_s3_bucket_policy" "allow_public_access" {
-  bucket = aws_s3_bucket.portfolio_bucket.id
+  bucket = aws_s_bucket.portfolio_bucket.id
+  # (NEW!) This line ensures the public access block is configured before the policy is applied.
+  depends_on = [aws_s_bucket_public_access_block.portfolio_bucket_public_access]
+
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
